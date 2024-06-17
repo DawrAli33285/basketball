b89d09da946893ba25be93890ff1185635e52b3a
Dawar <shahg33285@gmail.com>
2024-06-17 14:58:24 +0500

    updates

--- connection/connection.js
+++ connection/connection.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose');
-// lemightyeagle
 
-let connection = mongoose.connect('mongodb+srv://lemightyeagle:lemightyeagle@cluster0.7b9t5dx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+// MongoDB Atlas connection string
+const uri = 'mongodb+srv://lemightyeagle:lemightyeagle@cluster0.7b9t5dx.mongodb.net/test?retryWrites=true&w=majority';
 
-module.exports = connection;
+// Connection options to pass to mongoose.connect
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true, // optional, but recommended
+  useFindAndModify: false // optional, if you are using findOneAndUpdate or findOneAndDelete etc.
+};
+
+// Connect to MongoDB Atlas
+mongoose.connect(uri, options)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('MongoDB connection error:', err));
+
+module.exports = mongoose.connection;
