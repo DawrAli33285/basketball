9d884a0949949de4d6b6cf0c21aebe664292525c
Dawar <shahg33285@gmail.com>
2024-06-17 12:46:49 +0500

    Initial commit with project files

---
+++ index.js
@@ -0,0 +1,46 @@
+//imports
+const express=require('express')
+const app=express();
+const connection=require('./connection/connection')
+const cors=require('cors')
+require('dotenv').config();
+const authRoutes=require('./routes/auth/auth')
+const profileRoutes=require('./routes/profile/profile')
+const newsFeedRoutes=require('./routes/news feed/newsFeed')
+const videoRoutes=require('./routes/video/video')
+const bodyParser=require('body-parser')
+const coachRoutes=require('./routes/coach/coach')
+const serverless = require("serverless-http");
+//middlewares
+app.use(cors())
+app.use(express.json({
+    verify: (req, res, buffer) => req['rawBody'] = buffer, 
+  }));
+  
+app.use(express.urlencoded({
+    extended: true
+    }));
+
+
+
+//routes
+app.get('/',()=>{
+    return res.status(200).json({
+        message:"SUCCESS"
+    })
+})
+app.use(authRoutes)
+app.use(profileRoutes)
+app.use(newsFeedRoutes)
+app.use(videoRoutes)
+app.use(coachRoutes)
+//mongodb connection
+connection
+
+
+//server
+app.listen(process.env.PORT,()=>{
+    console.log(`Listening to port ${process.env.PORT}`)
+})
+
+
